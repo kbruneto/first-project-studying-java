856907d68d18058a4b443966bc5c0dd3565305b2
kbruneto <hizztxt@gmail.com>
2025-03-26 00:24:12 -0300

    commit-test-two

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,5 +1,5 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("Altered to testing");
+        System.out.println("Alterando para testar");
     }
 }
